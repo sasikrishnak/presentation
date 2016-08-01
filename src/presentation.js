8efe4c8dba4f4dc061cd51e56b88c8afc74e3bbf
Dorival Pedroso <pedroso@cpmech.com>
2016-08-02 02:21:49 +1000

    label in example changed

--- src/presentation.js
+++ src/presentation.js
@@ -5,7 +5,7 @@
 import Plotxy from 'jstar/src/canvas.plotxy.js';
 import Reveal from 'reveal/js/reveal.js';
 
-let p1 = new Plotxy('canvas-1', 'first plot', 'x', 'y');
+let p1 = new Plotxy('canvas-1', 'first plot', 'displacement [m]', 'force [kN]');
 p1.addCurve('series1a', [0.1, 0.2, 0.4, 0.8, 1.2], [0.15, 0.3, 0.8, 2.0, 4.0]);
 p1.winZoomFcn = Reveal.getScale;
 
